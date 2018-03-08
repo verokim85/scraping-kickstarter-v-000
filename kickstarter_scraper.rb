972eb3cf78e7abbf917872ae85e9fb68cfe98c48
Veronica Kim <vero.a.kim@gmail.com>
2018-03-08 03:23:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,11 +9,11 @@ def create_project_hash
 
   projects = {}
 
-  kickstarter.css("li.project.grid_4").each do |project|
-  title = project.css("h2.bbcard_name strong a").text
-  projects[title.to_sym] = {}
-end
+    kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
+
 
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
