3fffaed0dea9a80a7a403c49c14b07eae83b553b
Veronica Kim <vero.a.kim@gmail.com>
2018-03-08 03:17:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,10 +11,12 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
-  # location: project.css("ul.project-meta li a data-location").text 
+  # location: project.css("ul.project-meta li a data-location").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   binding.pry
 end
 
 
 create_project_hash
+
+Scraper.new.get_page
