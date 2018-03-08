61f73ad1c7c4dcb0e94fc7ec21095e2ea7acf152
Veronica Kim <vero.a.kim@gmail.com>
2018-03-08 03:03:44 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
+  # projects: kickstarter.css("li.project.grid_4")
   binding.pry
 end
 
